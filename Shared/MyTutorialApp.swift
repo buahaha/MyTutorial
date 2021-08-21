e5615245238327c60cc77d2db69acf3a139a1870
Szymon Błaszczyński <museyoucoulduse@gmail.com>
2021-08-21 11:06:09 +0200

    Initial Commit

---
+++ Shared/MyTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTutorialApp.swift
+//  Shared
+//
+//  Created by Szymon Błaszczyński on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
